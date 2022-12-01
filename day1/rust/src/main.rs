4d7246167d8d46f89df2bc6309b9ee0326f25128
Alex de la Cruz <alex@Alexs-MacBook-Air.local>
2022-12-01 15:37:28 +0100

    advent day1 1/2 rust done

--- day1/rust/src/main.rs
+++ day1/rust/src/main.rs
@@ -1,5 +1,13 @@
 use std::time::Instant;
-use std::fs;
+use std::fs::File;
+use std::io::{self, BufRead};
+use std::path::Path;
+
+fn read_lines<P>(filename: P) -> io::Result<io::Lines<io::BufReader<File>>>
+where P: AsRef<Path>, {
+    let file = File::open(filename)?;
+    Ok(io::BufReader::new(file).lines())
+}
 
 
 fn main() {
@@ -7,9 +15,28 @@ fn main() {
     let start = Instant::now();
 
     let input = "../input.txt";
-    let contents = fs::read_to_string(input)
-        .expect(&format!("Cannot open file {input}"));
+    let lines = read_lines(input)
+        .unwrap_or_else(|_| panic!("File '{input}'should be accesible"));
+
+    let mut max_calories = 0;
+    let mut sum_calories = 0;
+    for line in lines {
+        if let Ok(line) = line {
+            let line = line.parse::<u32>();
+            if let Ok(line) = line {
+                sum_calories += line;
+            }
+            else {
+                if max_calories < sum_calories {max_calories = sum_calories}
+                sum_calories = 0;
+            }
+        }      
+    }
+
+    println!("The number of calories of the Elf carrying the most is {max_calories}");
 
     let duration = start.elapsed();
     println!("\nTime elapsed in expensive_function() is: {:?}", duration);
 }
+
+
