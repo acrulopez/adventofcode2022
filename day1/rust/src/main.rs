8a8917159204b1c2af348ad685927d07d97edfe8
Alex de la Cruz <alex@Alexs-MacBook-Air.local>
2022-12-01 16:08:10 +0100

    advent day1 2/2 rust done

--- day1/rust/src/main.rs
+++ day1/rust/src/main.rs
@@ -9,12 +9,7 @@ where P: AsRef<Path>, {
     Ok(io::BufReader::new(file).lines())
 }
 
-
-fn main() {
-    println!("Starting execution!\n");
-    let start = Instant::now();
-
-    let input = "../input.txt";
+fn top1_calories(input: &str) -> u32 {
     let lines = read_lines(input)
         .unwrap_or_else(|_| panic!("File '{input}'should be accesible"));
 
@@ -22,9 +17,9 @@ fn main() {
     let mut sum_calories = 0;
     for line in lines {
         if let Ok(line) = line {
-            let line = line.parse::<u32>();
-            if let Ok(line) = line {
-                sum_calories += line;
+            let calories = line.parse::<u32>();
+            if let Ok(calories) = calories {
+                sum_calories += calories;
             }
             else {
                 if max_calories < sum_calories {max_calories = sum_calories}
@@ -33,7 +28,56 @@ fn main() {
         }      
     }
 
-    println!("The number of calories of the Elf carrying the most is {max_calories}");
+    max_calories
+}
+
+fn top3_calories(input: &str) -> u32 {
+    let lines = read_lines(input)
+        .unwrap_or_else(|_| panic!("File '{input}'should be accesible"));
+
+    let mut max_calories = [0,0,0];
+    let mut sum_calories = 0;
+    for line in lines {
+        if let Ok(line) = line {
+            let calories = line.parse::<u32>();
+            if let Ok(calories) = calories {
+                sum_calories += calories;
+            }
+            else {
+                if sum_calories < max_calories[2] {}
+                else if sum_calories > max_calories[0] {
+                    let aux = max_calories[0];
+                    let aux1 = max_calories[1];
+                    max_calories[0] = sum_calories;
+                    max_calories[1] = aux;
+                    max_calories[2] = aux1;
+                }
+                else if sum_calories > max_calories[1] {
+                    let aux1 = max_calories[1];
+                    max_calories[1] = sum_calories;
+                    max_calories[2] = aux1;
+                }
+                else {max_calories[2] = sum_calories}
+                sum_calories = 0;
+            }
+        }
+    }
+
+    max_calories[0] + max_calories[1] + max_calories[2]
+}
+
+
+fn main() {
+    println!("Starting execution!\n");
+    let start = Instant::now();
+
+    let input = "../input.txt";
+    
+    let max_calories = top1_calories(input);
+    let max3_calories = top3_calories(input);
+
+    println!("The number of calories of the Elf carrying the most is {max_calories}\n");
+    println!("The number of calories of the three Elf carrying the most is {max3_calories}");
 
     let duration = start.elapsed();
     println!("\nTime elapsed in expensive_function() is: {:?}", duration);
